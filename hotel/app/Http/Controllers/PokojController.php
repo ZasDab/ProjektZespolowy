8574cc96367c9a86196182f8b089b721c1bd268b
unknown <azasikowska@gmail.com>
2019-01-26 15:40:32 +0100

    Fix bad repo

---
+++ hotel/app/Http/Controllers/PokojController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pokoj;
+use Illuminate\Http\Request;
+
+class PokojController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pokoj  $pokoj
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pokoj $pokoj)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pokoj  $pokoj
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pokoj $pokoj)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pokoj  $pokoj
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pokoj $pokoj)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pokoj  $pokoj
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pokoj $pokoj)
+    {
+        //
+    }
+}
